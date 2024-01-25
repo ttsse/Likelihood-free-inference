539fe3c520dd9de5648a14b003adede5f1de5c2e
mayank05942 <mayanknautiyal88@gmail.com>
2024-01-25 12:57:20 +0100

    Updated the test to verify summary statistics generated by CNN1D model of Sciope

--- TTSSE_Project/tests/test_cnn_stats.py
+++ TTSSE_Project/tests/test_cnn_stats.py
@@ -1,7 +1,6 @@
 
 import numpy as np
 import pytest
-
 from TTSSE_Project.utilities.priors.uniform_prior import UniformPrior
 from TTSSE_Project.models.cnn_regressor import CNNModel
 
@@ -28,12 +27,8 @@ parameter_names = ['k1', 'k2']
 dmin = [0, 0]
 dmax = [1, 1]
 
-
-# In[3]:
-
-
 # Set up simulation
-# We use 10,000 samples in the training dataset to be able to run the notebook quickly, which is comparitively few
+# We use 10,000 samples in the training dataset to be able to run the test quickly, which is comparitively few
 # For accurate inference, the number should be 1 or 2 orders of magnitude more.
 
 n = 10000
@@ -46,35 +41,20 @@ validation_ts = np.expand_dims(np.array([simulator(p, n=100) for p in validation
 test_thetas = np.random.uniform(low=dmin, high=dmax, size=(n,2))
 test_ts = np.expand_dims(np.array([simulator(p, n=100) for p in test_thetas]), 2)
 
-
-
-# In[5]:
-
-
 # Set up training data in the right format
 train_ts = train_ts.transpose((0, 2, 1))
 validation_ts = validation_ts.transpose((0, 2, 1))
 test_ts = test_ts.transpose((0, 2, 1))
 obs_data = obs_data.reshape(1,1,100)
 
-
-# In[6]:
-
-
 # Set input and output shape for the CNN
 input_shape = (100,1)
 output_shape = 2
 
 
-# In[7]:
-
-
 # Set up the search space for inference
 dmin = [-2, -1]
 dmax = [4, 2]
-
-
-# In[8]:
 
 
 # Routines to normalize and denormalize data
@@ -91,31 +71,16 @@ def denormalize_data(data, dmin, dmax):
     return denorm
 
 
-# In[9]:
-
-
 normed_thetas = normalize_data(train_thetas, dmin, dmax)
 normed_thetas_val = normalize_data(validation_thetas, dmin, dmax)
 
-
-# In[10]:
-
-
-# Instantiate the model
 model_cnn = CNNModel(input_shape, output_shape)
-
-
-# In[11]:
 
 
 history_cnn = model_cnn.train(train_ts, normed_thetas, batch_size=256, 
                       epochs=20, verbose=0, learning_rate=0.001, 
                       early_stopping_patience=5,
                       validation_inputs=validation_ts, validation_targets=normed_thetas_val)
-
-
-# In[15]:
-
 
 def verify_model(model):
     pred_test = model.predict(test_ts)
@@ -129,7 +94,6 @@ def test_cnn_summary_stats():
     assert len(pred_summary_stats[0]) == 2, "CNN Summary Statistics test failed"
 
 
-# In[ ]:
 
 
 
